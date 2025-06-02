be1951d6285bae3fbad9c1c2c0f247e7c7ba6085
Duncan Walter <duncan1997@hotmail.nl>
2025-06-02 13:45:10 +0200

    added world map image alongside some additional styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import './App.css';
+
+// Package dependencies
 import {useState} from 'react';
 import axios from 'axios';
-// Wanneer de gebruiker op een knop klikt, wordt informatie over alle landen ter wereld opgehaald. Hiervoor zul je de documentatie van de REST Countries API moeten bekijken om erachter te komen welk endpoint het beste past. Wanneer er resultaten op de pagina worden getoond, verdwijnt de knop.
-//   Bovenaan de pagina staat de afbeelding van een wereldkaart (zie de map assets);
-// Wanneer de gebruiker op een knop klikt, wordt informatie over alle landen ter wereld opgehaald. Hiervoor zul je de documentatie van de API moeten bekijken om erachter te komen welk endpoint het beste past.
-//   Voor ieder land geef je het volgende weer:
-//   De naam van het land
-// De vlag van dat land
-// De zin: Has a population of [amount] people
-// Je zorgt dat de landen worden gesorteerd op populatie, van laag naar hoog;
-// De land-namen moeten worden weergegeven in een kleur die overeenkomt met het continent waar het land in ligt. Tip: maak hier een helper-functie voor die een regio-naam verwacht en bepaalt welke kleur het land moet krijgen. Een land ligt meestal in één van de volgende vijf regio's, maar uitzonderingen kunnen voorkomen:
-// Africa: blauw
-// Americas: groen
-// Asia: rood
-// Europe: geel
-// Oceania: paars
-// De styling mag je zelf bepalen. Je mag het voorbeeld namaken of een eigen huisstijl bedenken!
+
+// Assets
+import worldMapImage from './assets/world_map.png';
+
 function App() {
   async function getCountriesInformation() {
     const response = await axios.get("https://restcountries.com/v3.1/all");
@@ -37,6 +28,12 @@ function App() {
   const [hideGetCountriesButton, setHideCountriesButton] = useState(false);
 
   return (<>
+    <img
+      src={worldMapImage}
+      alt="An image of the world map"
+      className="world-map"
+    />
+    <h1>World regions</h1>
     <button type="button" onClick={getCountriesInformation} hidden={hideGetCountriesButton}>Toon alle landen</button>
     <div className="country-cards">
       {countriesInformation.map((countryInformation) => {return (
